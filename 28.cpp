796d55d1c8ba47fed6ef74859bebaabec0885b9d
Ayushi <ayushisinghfl21@gmail.com>
2023-05-19 23:33:13 +0530

    code

---
+++ 28.cpp
@@ -0,0 +1,16 @@
+// Recursion
+
+#include<iostream>
+using namespace std;
+void f( int i, int n){
+    if(i>n){
+        return ;
+    }
+    cout<<("ayushi");
+    f(i+1,n);
+}
+int main(){
+    int n;
+    cin>>n;
+    f(1,n);
+}

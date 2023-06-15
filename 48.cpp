8f25524b46e4067670acf9d4c7be99af8a424c91
Ayushi <ayushisinghfl21@gmail.com>
2023-06-15 20:24:32 +0530

    code

--- 48.cpp
+++ 48.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 
-using namespace std;
+bool isPalindrome(const std::string& str) {
+    std::string strCopy = str;
+    std::transform(strCopy.begin(), strCopy.end(), strCopy.begin(), ::tolower);
+    std::string::iterator left = strCopy.begin();
+    std::string::iterator right = strCopy.end() - 1;
 
-class Solution {
-public:
-    int maximumProduct(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int ans1 = nums[n-1] * nums[n-2] * nums[n-3];
-        int ans2 = nums[0] * nums[1] * nums[n-1];
-        return max(ans1, ans2);
+    while (left < right) {
+        if (*left != *right)
+            return false;
+        ++left;
+        --right;
     }
-};
+
+    return true;
+}
 
 int main() {
-    vector<int> nums = {4, 6, 3, 2, 8, 1, 9};
-    Solution solution;
-    int maxProduct = solution.maximumProduct(nums);
-    cout << "The maximum product is: " << maxProduct << endl;
+    std::string str;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+
+    if (isPalindrome(str))
+        std::cout << "The string is a palindrome." << std::endl;
+    else
+        std::cout << "The string is not a palindrome." << std::endl;
 
     return 0;
 }
